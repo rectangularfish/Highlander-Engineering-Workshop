5eeca811dec8b37f769ba880733ae31546d9d631
IDontEatFish <86937799+IDontEatFish@users.noreply.github.com>
2023-10-26 21:22:42 -0700

    comments

--- ESP8266/ESP8266.ino
+++ ESP8266/ESP8266.ino
@@ -1,50 +1,45 @@
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>     // Include the ESP8266 Wi-Fi library 
+                             // for network connectivity.
+                             
+#include <ESP8266HTTPClient.h> // Include the ESP8266 HTTP client 
+                               // library for making HTTP requests.
 
-
-WiFiClient client;
-
+WiFiClient client;           // Create a Wi-Fi client object for managing the Wi-Fi connection.
 
 // set the pin of the ultrasonic sensor
 const int trigPin = 12;
 const int echoPin = 14;
 
-//define sound velocity in cm/uS
+// define sound velocity in cm/uS
 #define SOUND_VELOCITY 0.034
 
 long duration;
 float distanceCm;
 float threshold = 20;
 
-
 void setup() {
   Serial.begin(115200); // Starts the serial communication
 
-  WiFi.begin("fisch4", "cmsptani1a");
-
-
+  // Connect to the Wi-Fi network
+  WiFi.begin(ssid, password); // ssid = network name and password = network password
   while (WiFi.status() != WL_CONNECTED) {
-
     delay(1000);
     Serial.println("Connecting..");
-
   }
-
   Serial.println("Connected to WiFi Network");
 
-
-
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  pinMode(echoPin, INPUT);  // Sets the echoPin as an Input
 }
 
 void loop() {
-  if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
+  if (WiFi.status() == WL_CONNECTED) { // Check WiFi connection status
 
     // Clears the trigPin
     digitalWrite(trigPin, LOW);
     delayMicroseconds(2);
-    // Sets the trigPin on HIGH state for 10 micro seconds
+
+    // Sets the trigPin on HIGH state for 10 microseconds
     digitalWrite(trigPin, HIGH);
     delayMicroseconds(10);
     digitalWrite(trigPin, LOW);
@@ -52,7 +47,7 @@ void loop() {
     // Reads the echoPin, returns the sound wave travel time in microseconds
     duration = pulseIn(echoPin, HIGH);
 
-    // Calculate the distance
+    // Calculate the distance in centimeters
     distanceCm = duration * SOUND_VELOCITY / 2;
 
     // Prints the distance on the Serial Monitor
@@ -60,23 +55,17 @@ void loop() {
     Serial.println(distanceCm);
 
     if (distanceCm < threshold) {
+      // If the distance is less than the threshold, send an HTTP GET request
 
-
-    HTTPClient http;
-
-        
+      HTTPClient http;
       http.begin(client, "http://192.168.3.62:8090/helloesp");
-      int httpCode = http.GET(); //Send the request
+      int httpCode = http.GET(); // Send the request
+      Serial.println("Sending a request");
+      http.end(); // Close the connection
 
-      Serial.println("sending a request");
-
-
-      http.end();   //Close connection
-
-      delay(10000);
+      delay(10000); // Wait for a while before sending another request
     }
   }
 
-
-  delay(50);
+  delay(50); // Delay to control the loop frequency
 }
--- main.py
+++ main.py
@@ -1,23 +1,22 @@
+# Import the Flask web framework and other required libraries
 from flask import Flask, request
-import pyautogui
-import webbrowser
-import time
+import pyautogui  # Import the pyautogui library for simulating keyboard and mouse actions
+import time  # Import the time library for adding delays
 
- 
+# Create a Flask web application instance
 app = Flask(__name__)
- 
-@app.route('/helloesp', methods = ['POST', 'GET'])
+
+# Define a route for handling HTTP requests at the '/helloesp' URL
+@app.route('/helloesp', methods=['POST', 'GET'])
 def helloHandler():
-
-
+    # Check if the incoming request method is 'GET'
     if request.method == 'GET':
-        
+        # Simulate pressing the Alt+Tab keys to switch between applications
         pyautogui.hotkey('alt', 'tab')
-        time.sleep(1)
+        time.sleep(1)  # Pause the program for 1 second
 
-
-
+        # Return a response indicating that a request has been sent
         return "Sent Request"
 
- 
-app.run(host='0.0.0.0', port= 8090) 
+# Start the Flask application and listen on all available network interfaces at port 8090
+app.run(host='0.0.0.0', port=8090)
